c0435ab3ac623c17a377e2270616ed075f47188e
Jeremy Banka <hello@jeremybanka.com>
2021-07-08 14:14:32 -0700

    🔧 add testing config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "rootDir": `.`,
+  "testEnvironment": `node`,
+  "modulePathIgnorePatterns": [
+    `dist`,
+  ],
+  "testRegex": `test.(ts|js)$`,
+  "coverageDirectory": `./coverage/`,
+  "collectCoverage": true,
+  "coverageReporters": [
+    `json`,
+    `html`,
+    `text`,
+    `text-summary`,
+  ],
+  "collectCoverageFrom": [
+    `src/**/*.{js,ts}`,
+    `tests/**/*.{js,ts}`,
+  ],
+}
