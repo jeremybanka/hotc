90e6849f1792f6ee79b782967053b34943c2f708
Jeremy Banka <hello@jeremybanka.com>
2021-07-27 08:24:03 -0700

    🏷  add 'Phase' type that represents a piece of a cardCycle

--- src/core/models/CardCycle.ts
+++ src/core/models/CardCycle.ts
@@ -1,12 +1,23 @@
-import { CardCycleId, CardGroupId } from "../util/Id"
+import { CardCycleId, CardGroupId, PlayerId, ZoneId } from "../util/Id"
+
+type TPhase =
+  | CardGroupId
+  | ZoneId
+  | Map<PlayerId, CardGroupId>
+  | Map<PlayerId, ZoneId>
+
+interface ICardCycleProps {
+  id?: string
+  phases: TPhase[]
+}
 
 export class CardCycle {
  id:CardCycleId
 
- cardGroupIds: CardGroupId[]
+ phases: TPhase[]
 
- constructor() {
-   this.id = new CardCycleId()
-   this.cardGroupIds = []
+ constructor({ id, phases } : ICardCycleProps) {
+   this.id = new CardCycleId(id)
+   this.phases = phases
  }
 }
