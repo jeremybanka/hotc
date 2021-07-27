99518b116874841c4c793333480b406ed4f7ae5e
Jeremy Banka <hello@jeremybanka.com>
2021-07-27 08:28:34 -0700

    🎨 cardGroup holds ids of cards, not the models themselves

--- src/core/models/CardGroup.ts
+++ src/core/models/CardGroup.ts
@@ -1,27 +1,21 @@
 import { immerable } from "immer"
 import { a } from "eny/build/node"
-import { CardGroupId, CardId, PlayerId } from "../util/Id"
+import { CardGroupId, CardId, CardValueId, PlayerId } from "../util/Id"
 import { privacy } from "./types"
 
 const { shuffle } = a
 
 export interface ICardGroupProps {
   id?: string
-  cards?: CardId[]
+  cardIds?: CardId[]
   ownerId?: PlayerId | null
   rotated?: 0
-}
-
-const CardGroupDefaults: ICardGroupProps = {
-  cards: [],
-  ownerId: null,
-  rotated: 0,
 }
 
 export class CardGroup {
   [immerable] = true
 
-  cards: CardId[]
+  cardIds: CardId[]
 
   class: string
 
@@ -35,20 +29,20 @@ export class CardGroup {
 
   constructor({
     id,
-    cards = [],
+    cardIds = [],
     ownerId = null,
     rotated = 0,
   }:ICardGroupProps) {
     this.id = new CardGroupId(id)
     this.class = `CardGroup`
-    this.cards = cards
+    this.cardIds = cardIds
     this.privacy = `public`
     this.ownerId = ownerId
     this.rotated = rotated
   }
 
   add(newCard: CardId, idx = 0): void {
-    this.cards.splice(idx, 0, newCard)
+    this.cardIds.splice(idx, 0, newCard)
   }
 }
 
@@ -59,10 +53,10 @@ export class Deck extends CardGroup {
     this.privacy = `public`
   }
 
-  shuffle = (): void => (this.cards = shuffle(this.cards))
+  shuffle = (): void => (this.cardIds = shuffle(this.cardIds))
 
   draw = (): CardId => {
-    const drawnCard = this.cards.shift()
+    const drawnCard = this.cardIds.shift()
     if (!drawnCard) throw new Error(`deck is empty`)
     return drawnCard
   }
