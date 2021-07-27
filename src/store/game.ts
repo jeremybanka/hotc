22ad86dda3241f0adefafe42aeeaa29dd4e0ea52
Jeremy Banka <hello@jeremybanka.com>
2021-07-27 08:31:26 -0700

    🎨 cleanup

--- src/store/game.ts
+++ src/store/game.ts
@@ -2,7 +2,6 @@ import create, { StoreApi } from 'zustand/vanilla'
 import produce from 'immer'
 import {
   GameId,
-  PlayerId,
   TrueId,
 } from '../core/util/Id'
 import {
@@ -17,13 +16,12 @@ import { IAction, IActionRequest } from '../core/actions/types'
 
 export interface GameSession {
   id: GameId
-  // set: CallableFunction
-  forEachPlayer: CallableFunction
+  forEachPlayer: (fn:() => void) => void
   actions: Record<string, IAction>
   actionLog: IActionRequest[]
   onPlayerJoin: CallableFunction
   getSocketOwner: CallableFunction
-  getPlayers: CallableFunction
+  getPlayers: () => Player[]
   dispatch(actionRequest:IActionRequest) : void
   identify(id:TrueId) : unknown
   cardsById: Record<string, Card>
@@ -40,117 +38,108 @@ export interface GameSession {
 
 const createGame
 = ()
-: StoreApi<GameSession> => create<GameSession>((set, get) => {
-  // const set = fn => setState(produce(fn))
-  console.log(``)
-  return ({
-    // set,
-    id: new GameId(),
-    actions: {},
-    actionLog: [],
-    cardsById: {},
-    cardCyclesById: {},
-    cardGroupsById: {},
-    cardValuesById: {},
-    playersById: {},
-    playerIdsByUserId: {},
-    playerIdsBySocketId: {},
-    zonesById: {},
-    zoneLayoutsById: {},
+: StoreApi<GameSession> => create<GameSession>((set, get) => ({
+  id: new GameId(),
+  actions: {},
+  actionLog: [],
+  cardsById: {},
+  cardCyclesById: {},
+  cardGroupsById: {},
+  cardValuesById: {},
+  playersById: {},
+  playerIdsByUserId: {},
+  playerIdsBySocketId: {},
+  zonesById: {},
+  zoneLayoutsById: {},
 
-    identify(id: TrueId) {
-      const idString = id.toString()
-      switch (id.of) {
-        case `Card`: return get().cardsById[idString]
-        case `CardCycle`: return get().cardCyclesById[idString]
-        case `CardGroup`: return get().cardGroupsById[idString]
-        case `CardValue`: return get().cardValuesById[idString]
-        case `Player`: return get().playersById[idString]
-        case `Zone`: return get().zonesById[idString]
-        case `ZoneLayout`: return get().zoneLayoutsById[idString]
-        default: throw new Error(`id of unknown entity`)
+  identify(id: TrueId) {
+    const idString = id.toString()
+    switch (id.of) {
+      case `Card`: return get().cardsById[idString]
+      case `CardCycle`: return get().cardCyclesById[idString]
+      case `CardGroup`: return get().cardGroupsById[idString]
+      case `CardValue`: return get().cardValuesById[idString]
+      case `Player`: return get().playersById[idString]
+      case `Zone`: return get().zonesById[idString]
+      case `ZoneLayout`: return get().zoneLayoutsById[idString]
+      default: throw new Error(`id of unknown entity`)
+    }
+  },
+
+  registerSocket: (socketId:string) => ({
+    to: (player:Player) => {
+      set(state => {
+        state.playerIdsBySocketId[socketId] = player.id.toString()
+      })
+    },
+  }),
+
+  onPlayerJoin: (userId:number, socketId:string) => {
+    set(state => {
+      const playerId = state.playerIdsByUserId[userId]
+      console.log(`playerIsAlreadyHere`, !!playerId)
+      if (playerId) {
+        state.playerIdsBySocketId[socketId] = playerId
+      } else {
+        const newPlayer = new Player(`displayName`, userId)
+        const playerId = newPlayer.id.toString()
+        state.playerIdsByUserId[userId] = playerId
+        state.playerIdsBySocketId[socketId] = playerId
+        state.playersById[newPlayer.id.toString()] = newPlayer
       }
-    },
+    })
+  },
 
-    registerSocket: (socketId:string) => ({
-      to: (player:Player) => {
-        set(state => {
-          state.playerIdsBySocketId[socketId] = player.id.toString()
-        })
-      },
-    }),
+  getSocketOwner: (socketId:string) =>
+    get().playersById[get().playerIdsBySocketId[socketId]],
 
-    // registerUserId: (userId:number) => ({
-    //   to
-    // }),
+  getPlayers: () => Object.values(get().playersById),
 
-    onPlayerJoin: (userId:number, socketId:string) => {
-      set(state => {
-        const playerId = state.playerIdsByUserId[userId]
-        console.log(`playerIsAlreadyHere`, !!playerId)
-        if (playerId) {
-          state.playerIdsBySocketId[socketId] = playerId
-        } else {
-          const newPlayer = new Player(`displayName`, userId)
-          const playerId = newPlayer.id.toString()
-          state.playerIdsByUserId[userId] = playerId
-          state.playerIdsBySocketId[socketId] = playerId
-          state.playersById[newPlayer.id.toString()] = newPlayer
-        }
-      })
-    },
+  forEachPlayer: (fn:CallableFunction): void => {
+    get().getPlayers().forEach(player => {
+      produce(player, (draft:Player) => fn(draft))
+    })
+  },
 
-    getSocketOwner: (socketId:string) =>
-      get().playersById[get().playerIdsBySocketId[socketId]],
+  install: (installer:CallableFunction): void => {
+    set(state => {
+      state.actions = {
+        ...state.actions,
+        ...installer(state),
+      }
+    })
+  },
 
-    getPlayers: () => Object.values(get().playersById),
+  dispatch: (actionRequest:IActionRequest): void => {
+    const { type, payload } = actionRequest
+    const { subjectId, targets, options } = payload
+    const action = get().actions[type]
 
-    forEachPlayer: (fn:CallableFunction): void => {
-      get().getPlayers().forEach(player => {
-        produce(player, (draft:Player) => fn(draft))
-      })
-    },
+    console.log(`action`, { ...payload, subjectId: subjectId?.toString() })
 
-    install: (installer:CallableFunction): void => {
-      set(state => {
-        state.actions = {
-          ...state.actions,
-          ...installer(state),
-        }
-      })
-    },
-
-    dispatch: (actionRequest:IActionRequest): void => {
-      const { type, payload } = actionRequest
-      const { from, targets = [], options } = payload
-      const action = get().actions[type]
-
-      console.log(`action`, { ...payload, from: from.toString() })
-
-      try {
-        const update = action.run({ from, targets, options })
-        set((state:GameSession) => {
-          state = { ...state, ...update }
-          state.actionLog.push(actionRequest)
-          const newPlayersById = { ...state.playersById }
-          Object.values(newPlayersById).forEach(player => {
-            const newPlayer = produce(player, draft => {
-              const imperative = draft.deriveImperative(actionRequest)
-              draft.imperativeLog.push(imperative)
-            })
-            newPlayersById[player.id.toString()] = newPlayer
+    try {
+      const update = action.run({ subjectId, targets, options })
+      set((state:GameSession) => {
+        state = { ...state, ...update }
+        state.actionLog.push(actionRequest)
+        const newPlayersById = { ...state.playersById }
+        Object.values(newPlayersById).forEach(player => {
+          const newPlayer = produce(player, draft => {
+            const imperative = draft.deriveImperative(actionRequest)
+            draft.imperativeLog.push(imperative)
           })
-          state.playersById = newPlayersById
-          return state
+          newPlayersById[player.id.toString()] = newPlayer
         })
-      } catch (error) {
-        console.log(error)
-        return error
-      }
-      console.log(`validated...`, get())
-    },
+        state.playersById = newPlayersById
+        return state
+      })
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+    // console.log(`validated...`, get())
+  },
 
-  })
-})
+}))
 
 export default createGame
