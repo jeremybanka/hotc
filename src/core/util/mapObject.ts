80669db0499cc62aad66c361c3c7fece618c70b3
Jeremy Banka <hello@jeremybanka.com>
2021-07-28 18:22:41 -0700

    🏷  add third generic type param for object key

--- src/core/util/mapObject.ts
+++ src/core/util/mapObject.ts
@@ -1,16 +1,16 @@
 const mapObject
-= <I, O> (
-  obj: Record<string, I>,
+= <K extends (string|number), I, O> (
+  obj: Partial<Record<K, I>>,
   fn: (val: I) => O
 )
-: Record<string, O> => {
-  const newObj = {}
-  const entries = Object.entries(obj)
+: Partial<Record<K, O>> => {
+  const newObj = { } as Record<K, O>
+  const entries = Object.entries(obj) as [K, I][]
   const newEntries = entries.map(entry =>
-    [entry[0], fn(entry[1])] as [string, O]
+    [entry[0], fn(entry[1])] as [K, O]
   )
-  newEntries.forEach(entry => {
-    newObj[entry[0]] = entry[1]
+  newEntries.forEach((entry:[K, O]) => {
+    newObj[entry[0] as K] = entry[1]
   })
   return newObj
 }
